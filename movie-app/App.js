d4c70e60ad398827f15ab69903172458149a6ab3
Hunter Tsai <as5823934@gmail.com>
2018-08-13 20:32:39 -0700

    implement search function

--- movie-app/App.js
+++ movie-app/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import { StyleSheet, FlatList, ActivityIndicator, Text, View, TouchableOpacity, Image  } from 'react-native';
-import db from './tmdb'
+import { StyleSheet, FlatList, TextInput, ActivityIndicator, Text, View, TouchableOpacity, Image  } from 'react-native';
+import { Icon, Button } from 'react-native-elements';
 
 export default class App extends React.Component {
   constructor(props){
     super(props);
     this.state ={ 
       isLoading: true,
-      dataSource: []
+      dataSource: [],
+      listToDisplay: [],
+      searchKeyword: '',
+      option: 'now_playing'
     }
   }
 
   componentDidMount(){
-    return fetch('https://api.themoviedb.org/3/discover/movie?api_key=97b14acad53b20d73f2eda02d46da480&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
+    const option = this.state.option
+    return fetch(`https://api.themoviedb.org/3/movie/${option}?api_key=97b14acad53b20d73f2eda02d46da480&language=en-US&page=1`)
       .then((response) => response.json())
       .then((responseJson) => {
         this.setState({
@@ -28,12 +32,19 @@ export default class App extends React.Component {
       });
   }
 
+  // changeOption = () => {
+  //   this.setState({
+  //     option: 'popular'
+  //   })
+  // }
+
   renderTop() {
     return(
-      <View style={{alignItems: 'center', padding: 20, backgroundColor: 'yellow', marginBottom: 8}}>
+      <View style={{padding: 20, alignItems: 'center' , backgroundColor: 'yellow', marginBottom: 6}}>
         <Text style={{fontSize: 30}}>
           Movie App
         </Text>
+        <Button title={'change'} onPress={()=> this.changeOption}/>
       </View>
     );
   }
@@ -76,6 +87,99 @@ export default class App extends React.Component {
     )  
   }
 
+  onPress = () => {
+    if(this.state.searchKeyword === '') {
+      return;
+    }
+    const tempArr = this.state.entireUserList.filter(x =>
+      this.isSearched(x.title.toLowerCase(), this.state.searchKeyword)
+    );
+
+    this.setState({ listToDisplay: tempArr });
+  }
+
+  autoSearch = (keyword) => {
+    this.setState({ searchKeyword: keyword });
+
+    const tempArr = this.state.dataSource.filter(x =>
+      this.isSearched(`${x.title.toLowerCase()}`, keyword.toLowerCase())
+    );
+
+    this.setState({ listToDisplay: tempArr });
+  }
+
+  isSearched = (target, keyword) => {
+    if (target.search(keyword) >= 0) {
+      return true;
+    }
+    return false;
+  }
+
+  _deleteSearchText = () => {
+    this.setState({ searchKeyword: '' });
+    this._textInput.setNativeProps({ text: '' });
+  }
+
+  _returnCloseIcon = () => (
+    <TouchableOpacity onPress={() => this._deleteSearchText()} flex={2} paddingRight={10}>
+      <Icon name='close' marginRight={10} />
+    </TouchableOpacity>
+  )
+
+  _returnSearchIcon = () => {
+    console.log('there is no search texts');
+    return (
+      <TouchableOpacity onPress={() => this.onPress()} flex={2} paddingRight={10}>
+        <Icon name='search' marginRight={10} />
+      </TouchableOpacity>
+    );
+  }
+
+  renderSearchBar() {
+    return (
+      <View style={styles.searchBarStyle} >
+        <TextInput
+          paddingLeft={10}
+          autoCapitalize='none'
+          flex={5}
+          placeholder='search'
+          fontSize={18}
+          autoCorrect={false}
+          ref={component => (this._textInput = component)}
+          onChangeText={(value) => {
+            this.autoSearch(value);
+            this.setState({ searchKeyword: value });
+            console.log(value);
+          }
+          }
+          returnKeyType='search'
+        />
+
+        {this.state.searchKeyword.trim() === '' ? this._returnSearchIcon() : this._returnCloseIcon()}
+      </View>
+    );
+  }
+
+  renderList = () => {
+    if (this.state.searchKeyword === '') {
+      return (
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={this.renderItem}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      )
+    } else {
+      return (
+        <FlatList
+          data={this.state.listToDisplay}
+          renderItem={this.renderItem}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      )
+    }
+  }
+
   render(){
     if(this.state.isLoading){
       return(
@@ -84,15 +188,17 @@ export default class App extends React.Component {
         </View>
       )
     }
-
+    console.log(this.state.listToDisplay)
     return(  
-      <View style={{flex: 1, paddingTop:20, backgroundColor: '#000'}}>
+      <View style={{flex: 1, backgroundColor: '#000', flexDirection: 'column'}}>
       {this.renderTop()}
-        <FlatList
-          data={this.state.dataSource}
+      {this.renderSearchBar()}
+      {this.renderList()}
+        {/* <FlatList
+          data={this.state.listToDisplay}
           renderItem={this.renderItem}
           keyExtractor={(item, index) => index.toString()}
-        />
+        /> */}
       </View>
     );
   }
@@ -104,5 +210,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  searchBarStyle: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    flexDirection: 'row',
+    borderBottomColor: '#efefef',
+    borderBottomWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white'
   },
 });
